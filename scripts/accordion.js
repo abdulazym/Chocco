c257342fe12be985ce4643a869df338489f48c15
Dadaev Abdulazym <adadaev2016@gmail.com>
2020-07-12 14:57:38 +0300

    сделал ops и горизонтальный аккордеон

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,9 +3,20 @@
     
     team.addEventListener('click', function(e){
         e.preventDefault();
-        let link = e.target;
+        let link = e.target.closest('.team__name-link');
         let listItem = e.currentTarget;
-        let name = document.querySelector('.team__name-link');
+        // let currentItem = link.closest('.team__item');
+        // let info = currentItem.querySelector('.info');
+        
+        // if (currentItem.classList.contains('team-accordion--active')){
+        //     info.style.height = 0; 
+        //     currentItem.classList.remove('team-accordion--active');
+        // }else{
+        //     currentItem.classList.add('team-accordion--active');
+        //     info.style.height = info.scrollHeight + 'px';
+        // }
+        
+        
 
         if(link.classList.contains('team__name-link')){
             const active = listItem.querySelector('.team__item.team-accordion--active');
@@ -18,11 +29,12 @@
             }
 
                 //открытие
-            if(!active || active.querySelector('.team__name-link') != link){//втоорое условие??
+            if(!active || active.querySelector('.team__name-link') != link){
                 let currentElement = link.closest('.team__item');
                 currentElement.classList.add('team-accordion--active');
                 let info = currentElement.querySelector('.info');
                 info.style.height = info.scrollHeight + 'px';
             }
         }
-    })
+        
+})
